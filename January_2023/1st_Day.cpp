059e79c1fffa81da6d4b79bc1a7b6b6682ff009a
Sanyam Jain <sanyamjain6667@gmail.com>
2023-01-01 21:00:54 +0530

    Create 1st_Day.cpp
    Let's start the Hustle

---
+++ January_2023/1st_Day.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"---------------- Let's Hustle For 2023 ----------\n";
+    cout<<" ________________Keep Hustling , Keep Growing ________________";
+    
+    return 0;
+}
+
+
+/* 
+                  OUTPUT
+---------------- Let's Hustle For 2023 ---------- 
+ ________________Keep Hustling , Keep Growing ________________
+
+ */
