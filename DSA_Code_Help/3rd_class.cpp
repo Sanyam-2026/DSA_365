331afd61b612e1d3b3c15835aaa9e189ec666349
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-01 22:45:14 +0530

    Create 3rd_class.cpp

---
+++ DSA_Code_Help/3rd_class.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+     // First Question 
+     int bro_num;
+     cin>>bro_num;
+     if (bro_num == 0)
+     {
+         cout<<"Baat Ban jayegi ";
+     }
+      else
+     {
+         cout<<"Baat  nhi banegi   ";
+     }
+  
+   // Question 2 
+     int num;
+    
+     if ( cin>> num)
+     {
+         cout<< num;
+     }
+     
+     return 0;
+}
