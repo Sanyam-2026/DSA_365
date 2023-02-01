883eeaf8ff736d6bf66e80682406afc19893c1f3
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-01 23:44:53 +0530

    Create Full_Pyramid.cpp

---
+++ DSA_Code_Help/3rd_class/Full_Pyramid.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    
+     int row_count;
+cout<<"Enter the No. of row in Full pyramid pattern"<<endl;
+cin >>row_count;
+
+for (int row = 0; row < row_count; row++)
+{
+       for ( int col = 0; col < row_count-row; col++)
+    {
+     cout<<" ";
+    }
+    for (int star = 0; star < row+1; star++)
+    {
+      cout<<"*";
+    }
+  
+    cout<<endl;
+}
+
+     
+     return 0;
+}
