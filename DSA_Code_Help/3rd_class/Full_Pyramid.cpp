00338eeaeb1271eda24ce4eaffebcc9724cb8256
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-03 21:26:06 +0530

    Update Full_Pyramid.cpp

--- DSA_Code_Help/3rd_class/Full_Pyramid.cpp
+++ DSA_Code_Help/3rd_class/Full_Pyramid.cpp
@@ -11,13 +11,13 @@ cin >>row_count;
 
 for (int row = 0; row < row_count; row++)
 {
-       for ( int col = 0; col < row_count-row; col++)
+       for ( int col = 0; col < row_count-row-1; col++)
     {
      cout<<" ";
     }
     for (int star = 0; star < row+1; star++)
     {
-      cout<<"*";
+      cout<<" *";
     }
   
     cout<<endl;
