e9a28b9a39cb6ddf410e481ae174ebac6c733ca6
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-04 13:14:40 +0530

    Create Floyed_Triangle.cpp

---
+++ DSA_Code_Help/Pattern/Floyed_Triangle.cpp
@@ -0,0 +1,46 @@
+/*
+Enter the no of rows
+6
+ 1 
+ 2  3 
+ 4  5  6 
+ 7  8  9  10 
+ 11  12  13  14  15 
+ 16  17  18  19  20  21 
+
+
+
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    
+     int n;
+cout<<"Enter the no of rows"<<endl;
+cin >>n;
+  int var = 1;
+
+ 
+for (int row = 0; row < n; row++)
+{
+ 
+   for(int col = 0; col<  row+1;col++){
+     cout<<" "<<var<<" ";
+     var++;
+     
+     }
+  
+    cout<<endl;
+
+
+ 
+  }
+ 
+ return 0;
+}
