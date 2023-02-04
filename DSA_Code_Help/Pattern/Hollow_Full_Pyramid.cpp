b906e69eaa6803021bf997c011035ac4a3fe6c68
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-04 12:14:03 +0530

    Create Hollow_Full_Pyramid.cpp

---
+++ DSA_Code_Help/Pattern/Hollow_Full_Pyramid.cpp
@@ -0,0 +1,46 @@
+/*
+Enter the no of rows
+5
+     *
+    * *
+   *   *
+  *     *
+ * * * * *
+ 
+ */
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    
+     int n;
+cout<<"Enter the no of rows"<<endl;
+cin >>n;
+ 
+for (int row = 0; row < n; row++)
+{
+   
+       for ( int col = 0; col <n-row; col++)
+    {
+      //  Condition for print all star in First line and print space in other lines 
+       if(  row == 0  ||col== 0 || col == n-row-1)
+         cout<<"*";
+      else
+         cout<<" ";
+ 
+    }
+  
+   
+    cout<<endl;
+}
+ 
+ 
+ 
+
+     
+     return 0;
+}
