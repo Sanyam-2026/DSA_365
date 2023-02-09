f80e115b830b0ecbc2c06ff7aaf29dfad6422908
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-09 11:03:12 +0530

    Update count_Set_Bits.cpp

--- DSA_Code_Help/Function/count_Set_Bits.cpp
+++ DSA_Code_Help/Function/count_Set_Bits.cpp
@@ -1,1 +1,33 @@
+/*
+Enter the no. to count set bit
+3
+No. of Set bit in 3 is : 2
+*/
+#include<iostream>
+using namespace std;
+void count_Set_Bit(int n ){
+  cout<<"No. of Set bit in "<<n<<" is : ";
+  int count =0;
+while(n!= 0){
 
+   if(n&1){
+     count++;
+   }
+   
+ n =  n >> 1;
+}
+ cout<<count;
+  
+}
+int main()
+{
+ 
+     int n;
+cout<<"Enter the no. to count set bit"<<endl;
+cin >>n;
+count_Set_Bit(n);
+   
+   
+   
+     return 0;
+}
