be39f92812522f22b17c9e3acbb00029064306fb
Sanyam Jain <sanyamjain6667@gmail.com>
2023-02-10 16:48:25 +0530

    Update print_prime_In_Range.cpp

--- DSA_Code_Help/Function/print_prime_In_Range.cpp
+++ DSA_Code_Help/Function/print_prime_In_Range.cpp
@@ -1,5 +1,37 @@
 /*
-
-
-
+Enter the range to print Prime No .  
+5
+2 3 5 
 */
+#include<iostream>
+using namespace std;
+
+ //  Function to  check Prime no  
+
+ bool check_Prime ( int n){
+
+  for(int i=2; i<n;i++){
+    if(n%i==0){
+    return false; 
+    }
+    }
+    return true;
+  }
+    
+
+int main()
+{
+
+    
+   int no;
+  cout<<"Enter the range to print Prime No .  "<<endl;
+  cin>>no;
+   for(int i =2 ; i<=no;i++){
+     if(check_Prime(i)){
+       cout<<i<<" ";
+       
+     }
+     
+   }
+ return 0;
+}
